3fd817c0d98067390cdfa8a11811ac7f29910b88
Edgar Jacquemoud <59689456+Nyuu-s@users.noreply.github.com>
2025-01-21 20:33:16 +0100

    rdp id division

--- parsing/Cobarse.py
+++ parsing/Cobarse.py
@@ -1,46 +1,89 @@
 from Coblex import lex
 from CobolTokens import token_value, is_token_of_kind, token_line
-# from parsing.ASTser import Tree, Node, NodeKinds
+import ASTser 
 class RecDParser:
 
     def __init__(self, tokens = []):
         self.tokens = tokens 
         self.current = None
+        
          
     def nextToken(self):
         if len(self.tokens) > 0:
             self.current = self.tokens.pop(0)
         else:
             self.current = None
-        
-    def matchLookAHeadX(self, matchstr, offset = 0):
-        return token_value(self.tokens[offset]) == matchstr
-    def syntax_error(self, token, expected):
-        print("ERROR invalid token ", token_value(token), "on line ", token_line(token), f" expected any of [{ ','.join(expected) }]" )
-              
-    def matchAnyTokens(self, token, others, doPrintError=True):
-        for other in others:
-            if token_value(token) == other:
+
+    def skipXtoken(self, x):
+        for i in range(x):
+            if i == x-1:
                 self.nextToken()
-                print(token)
+            else:
+                self.tokens.pop(0)
+
+    def matchTokenWithAny(self, token, *others):
+        for other in others:
+            if other == token_value(token):
+                self.nextToken()
                 return True
-        if doPrintError:
-            self.syntax_error(token , others)
+        self.syntax_error(token, [el for el in others])
+        return False
     
-    def program(self):
-        self.nextToken()
-        if not self.matchAnyTokens(self.current, ["ID", "IDENTIFICATION"]):
-            return
-        if not self.matchAnyTokens(self.current, ["DIVISION"]):
-            return
-        if not self.matchAnyTokens(self.current, ["."]):
-            return
+    def matchLookAheadWith(self, la_offset, other):
+        return token_value(self.tokens[la_offset]) == other
+
+    def syntax_error(self, token, expected):
+        print("ERROR invalid token", token_value(token), "on line", token_line(token), f"expected any of [{ ','.join(expected) }]" )
         
-        print("done")
-
     
-   
+    def program_id_cobol_source_program(self) -> ASTser.ProgramID:
 
+        progid = ASTser.ProgramID()
+        if not self.matchTokenWithAny(self.current, "PROGRAM-ID"):
+            return progid
+        self.matchTokenWithAny(self.current, ".")
+        if is_token_of_kind(self.current, "SYMBOL"):
+            progid.ID = token_value(self.current)
+            self.nextToken()
+        if not token_value(self.current) == ".":
+            self.syntax_error(self.current, ".")
+            return progid
+        
+        #optional part
+        reading_offset = 0
+        if self.matchLookAheadWith(reading_offset, "IS"):
+            reading_offset += 1 
+            if self.matchTokenWithAny(self.tokens[reading_offset], "INITIAL"):
+                progid.initial = True
+                reading_offset += 1
+            else:
+                return None
+            if self.matchLookAheadWith(reading_offset, "PROGRAM"):
+                reading_offset += 1
+        elif self.matchLookAheadWith(reading_offset, "INITIAL"):
+            progid.initial = True
+            reading_offset += 1
+            if self.matchLookAheadWith(reading_offset, "PROGRAM"):
+                reading_offset += 1
+
+        if self.matchLookAheadWith(reading_offset, "."):
+            reading_offset += 1
+            
+        self.skipXtoken(reading_offset)
+        return progid
+
+    def cobol_source_program(self):
+        self.nextToken()
+        if not self.matchTokenWithAny(self.current, "ID", "IDENTIFICATION"):
+            return
+        if not self.matchTokenWithAny(self.current, "DIVISION"):
+            return
+        if not self.matchTokenWithAny(self.current, "."):
+            return
+        programid = self.program_id_cobol_source_program()
+        if programid != None:
+            print(programid.ID, programid.initial)
+        
 
 
 def main():
@@ -50,7 +93,7 @@ def main():
     # tree = Tree()
     stack = []
     # for i, current in enumerate(tokens):
-    parser.program()
+    parser.cobol_source_program()
 
     
 
