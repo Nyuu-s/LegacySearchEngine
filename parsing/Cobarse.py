9aec08cf76e1cb2ba38d7bbf02fce458fc959e0b
Edgar Jacquemoud <cookiesarenightmares@gmail.com>
2025-02-02 23:37:18 +0100

    testing ll1 parsing table impl

--- parsing/Cobarse.py
+++ parsing/Cobarse.py
@@ -1,12 +1,61 @@
 from Coblex import lex
-from CobolTokens import token_value, is_token_of_kind, token_line
+from CobolTokens import token_value, is_token_of_kind, token_line, _KINDS
 import ASTser 
 class RecDParser:
+
+    S = 0
+    A = 1
+    B = 2
+    C = 3
+    D = 4
+    E = 5
+    F = 6
+    SYMBOL = _KINDS[2]
+    def peek(self):
+        return self.stack[len(self.stack)-1]
+    def pop(self):
+        self.stack.pop()
+    def add(self, el):
+        self.stack.append(el)
 
     def __init__(self, tokens = []):
         self.tokens = tokens 
         self.current = None
+        self.table = [
+            {
+                "ID": [self.fA, "ID"],
+                "IDENTIFICATION": False
+            },
+            {
+                "PROG-ID": ""
+            },
+            {
+                ".": "",
+                self.SYMBOL: ""
+            },
+            {
+                self.SYMBOL: ""
+            }
+        ]
+        self.stack = []
+    def fA(self):
+        print("parsing A rule")
+    def source(self):
+        self.stack.append("$")
+        self.nextToken()
+        if things := self.table[self.S].get(token_value(self.current), False):
+            for thing in things:
+                self.stack.append(thing)
+        else:
+            self.syntax_error(self.current, "something")
+            
+        while self.peek() == token_value(self.current):
+            self.nextToken()
+            self.pop()
+
+        self.peek()()
         
+    
          
     def nextToken(self):
         if len(self.tokens) > 0:
@@ -35,13 +84,12 @@ class RecDParser:
     def syntax_error(self, token, expected):
         print("ERROR invalid token", token_value(token), "on line", token_line(token), f"expected any of [{ ','.join(expected) }]" )
         
+    def Optional_dot(self):
+        if token_value(self.current) == ".":
+            self.nextToken()
     
     def program_id_cobol_source_program(self) :
-        def Optional1():
-            if token_value(self.current) == ".":
-                self.nextToken()
         def Optional2():
-            
             if Optional3() and not token_value(self.current) == "INITIAL":
                 self.syntax_error(self.current, ["INITIAL"])
                 return None
@@ -61,12 +109,12 @@ class RecDParser:
         # progid = ASTser.ProgramID()
         if not self.matchTokenWithAny(self.current, "PROGRAM-ID"):
             return None
-        Optional1()
+        self.Optional_dot()
         if is_token_of_kind(self.current, "SYMBOL"):
             # progid.ID = token_value(self.current)
             self.nextToken()
         Optional2()
-        Optional1()
+        self.Optional_dot()
         
 
     def cobol_source_program(self):
@@ -90,7 +138,7 @@ def main():
     # tree = Tree()
     stack = []
     # for i, current in enumerate(tokens):
-    parser.cobol_source_program()
+    parser.source()
 
     
 
