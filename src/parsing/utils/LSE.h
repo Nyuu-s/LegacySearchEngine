f891b822ecb008be3e1e42ae91f5c3d872cbb6eb
Edgar Jacquemoud <cookiesarenightmares@gmail.com>
2025-07-14 21:16:59 +0200

    generic dynamic array attempt

--- src/parsing/utils/LSE.h
+++ src/parsing/utils/LSE.h
@@ -22,172 +22,122 @@
 
 
 
+LSEArena* arena_create(size_t size);
+void* arena_alloc(LSEArena* arena, size_t size);
+void arena_release(LSEArena* arena);
+
 typedef struct LSEArena
 {
     char* data;
     size_t offset;
     size_t capacity;
-    // struct LSEArena* next; // remove ? maybe keep it simple
 
 } LSEArena;
 
-LSEArena arena_create(size_t size);
-void* arena_alloc(LSEArena* arena, size_t size);
-void arena_release(LSEArena* arena);
 
 
+LSEArena* arena_create(size_t size){
+    LSEArena* arena = (LSEArena*) malloc(sizeof(LSEArena) + size);
+    if (!arena) { return NULL; }
+    arena->data = (char*) (arena + 1);
+    arena->capacity = size;
+    arena->offset = 0;
+    memset(arena->data, 0, size);
 
-LSEArena arena_create(size_t size){
-    LSEArena arena = {0};
-    arena.data = (char*) malloc(size);
-    if(arena.data){
-        arena.capacity = size;
-        memset(arena.data, 0, size);
-    }
     return arena;
 }
 
 void* arena_alloc(LSEArena* arena, size_t size){
-    void* result = NULL;
-    size_t allignment = (size + 7) & ~7;
-    if(arena->offset + allignment > arena->capacity){
+    if (!arena) { return NULL; }
+    size_t aligned_size  = (size + 7) & ~7;
+    if(arena->offset + aligned_size > arena->capacity){
         return NULL;
     }
-    result = arena->data + arena->offset;
-    arena->offset += size;
+    void* result = arena->data + arena->offset;
+    arena->offset += aligned_size;
     return result;
 }
-//TODO arenaRelease
+
+void arena_release(LSEArena* arena)
+{
+    if(!arena){return;}
+    free(arena);
+}
 
 
 // #############################
 // #		Data Structures
 // #############################
 #define DYNA_INITIAL_CAPACITY 256
-#define DYNA_INIT(T, cap, arena) (T*)(dyna_init(cap, sizeof(T), arena))
-typedef struct DYNA_ARRAY
-{
-    size_t size;
+
+
+typedef struct LSEDYNA{
+    void* data;
     size_t capacity;
-    size_t item_size;
-    LSEArena* arena;
-} DYNA_ARRAY;
+    size_t length;
+    LSEArena* region;
+} LSEDYNA;
 
-void* dyna_init(size_t capacity, size_t item_size, LSEArena* arena){
-    size_t total_size = sizeof(DYNA_ARRAY) + capacity * item_size;
-    if(arena == NULL || arena->offset + total_size > arena->capacity) return NULL;
 
-    DYNA_ARRAY* array = arena_alloc(arena, total_size);
-    if(!array) return NULL;
-    
-    array->arena = arena;
-    array->capacity = capacity;
-    array->size = 0;
-    array->item_size = item_size;
-    array += 1;
-    return array;
+void* dyna_init(LSEArena* arena, size_t capacity, size_t item_size){
+    if(!arena) {return NULL;}
+    if (item_size == 0) { return NULL; }
+
+    if(capacity == 0) {capacity = DYNA_INITIAL_CAPACITY;}
+    LSEDYNA* res = (LSEDYNA*) arena_alloc(arena, (capacity * item_size) + sizeof(LSEDYNA));
+    if(!res){return NULL;}
+    res->capacity = capacity;
+    res->length = 0;
+    res->region = arena;
+    res->data = res + 1;
+    return res->data;
 }
-void* dyna_resize_if_needed(DYNA_ARRAY* array, size_t item_count){
 
-    //check if desired capacity do not exeed array capacity
-    //yes -> rturn the array as is
-    //no -> proceed
-    if(array->size + item_count <= array->capacity){
-        return array;
-    }
+void dyna_free(void* arr_data) {
+    if (!arr_data) { return; }
+    LSEDYNA* array = ((LSEDYNA*) arr_data) - 1;
+    arena_release(array->region);
+}
 
-    //check if desired capacity exeeds arenas available region (capacity - used)
-    //no  -> extends last allocation by bumbing arenas offset
-    //yes -> create new arena, copy data and free old
-    size_t new_item_size = array->item_size * item_count;
-    if (new_item_size > (array->arena->capacity - array->arena->offset))
+void* dyna_append_item(void* arr_data, void* ptr_item, size_t item_size){
+    if(!arr_data || !ptr_item){return NULL;}
+    LSEDYNA* array = ((LSEDYNA*) arr_data)-1;
+    if (array->length >= array->capacity)
     {
-        // create new arena
+        size_t new_cap = array->capacity == 0 ? DYNA_INITIAL_CAPACITY : array->capacity * 2;
+        // Only necessary for append_many function where length can grow beyonf capacity
+        // while (new_cap < array->length)
+        // {
+        //     new_cap *= 2;
+        // }
+        if(!array->region) {return NULL;}
+        size_t new_size = sizeof(LSEDYNA) + new_cap * item_size;
+        if(array->region->capacity < new_size){
+            LSEArena* new_arena = arena_create(new_size);
+            if(!new_arena){return NULL;}
+            LSEDYNA* new_arr = (LSEDYNA*) arena_alloc(new_arena, new_size);
+            if(!new_arr){
+                arena_release(new_arena);
+                return NULL;
+            }
+            memcpy(new_arr, array, (array->length * item_size) + sizeof(LSEDYNA));      
+            new_arr->data = new_arr + 1;
+            new_arr->capacity = new_cap;
+            new_arr->length = array->length;
+            new_arr->region = new_arena;
+            dyna_free(arr_data);
+            array = new_arr;
+        }
+        else
+        {
+            array->capacity = new_cap;
+        }
+        
     }
-    
-    //check if desired capacity exeeds arena's capacity
-    //yes -> create new arena, copy data back return new pointer to array, free the old array /!\ will invalidate pointer to the old array
-    //no -> proceed
-
-    //return the pointer to the data in the array
+    memcpy((char*)array->data + (array->length * item_size), ptr_item, item_size);
+    array->length++;
+    return array->data;
 }
-void* dyna_append(void* array, void* element){
-    //todo: check if array is SETTLED, no more appending allowed
-
-    array = dyna_resize_if_needed();
-    DYNA_ARRAY* header = (DYNA_ARRAY*)array - 1; 
-
-    //copy value to end of array and avoid using memcpy, maybe less perf but usable in more env
-    char* dest = (char*)array + header->size * header->item_size;
-    char* src =  (char*)element;
-    for(size_t i = 0; i<header->item_size; i++){
-        dest[i] = src[i];
-    }
-    header->size++;
-}
-
-void t(){
-   int* p = dyna_init(10, sizeof(int), NULL);
-   int a =5;
-   dyna_append(p, &a);
-}
-
-// #define ARRAY(T, a) (T*)dyna_init(sizeof(T), initial_capacity, a)
-// #define GET_DYNA_HEADER(arr) ((DYNA_HEADER*) (arr) - 1)
-
-// #define DYNA_APPEND(arr, value)\
-//     ((arr) = dyna_check_capacity_and_realloc(arr, 1, sizeof(value)),\
-//     (a)[GET_DYNA_HEADER(arr)->size] = (value),\
-//     &(a)[GET_DYNA_HEADER(arr)->size++])
-    
-
-// void* dyna_check_capacity_and_realloc(void* array, size_t item_count, size_t item_size){
-//     DYNA_HEADER* h = GET_DYNA_HEADER(array);
-//     size_t desired_cap = h->size + item_count;
-
-//     if(h->capacity < desired_cap){
-//         size_t new_cap = h->capacity * 2;
-//         while(h->capacity < desired_cap){
-//             new_cap *= 2;
-//         }
-//         size_t new_size = sizeof(DYNA_HEADER) + new_cap * item_size;
-//         DYNA_HEADER* new_h = arena_alloc(h->arena, new_size);
-//         if(!new_h){
-//             h = 0;
-//             return h;
-//         }
-//         size_t old_size = h->size * item_size + sizeof(DYNA_HEADER);
-//         // memcpy(new)
-//     }
-//     else {
-//        h += 1; 
-//     }
-//     return h;
-
-// }
-// void* dyna_init(size_t item_size, size_t capacity, LSEArena* arena ){
-//     void* ptr = 0;
-//     size_t size = item_size * capacity + sizeof(DYNA_HEADER);
-//     DYNA_HEADER *h = arena_alloc(arena, size);
-//     if(h){
-//         h->capacity = capacity;
-//         h->size = 0;
-//         h->arena = arena;
-//         ptr = h + 1;
-//     }
-//     return ptr;
-// }
-// //todo append
-// typedef struct DYNA_HEADER
-// {
-//     size_t capacity;
-//     size_t size;
-//     LSEArena* arena;
-// } DYNA_HEADER;
-
-
-
-
 
 
 // #############################
