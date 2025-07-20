3909839d0aeef7c35fc302ab08e77d612fd7bb75
Edgar Jacquemoud <cookiesarenightmares@gmail.com>
2025-07-21 00:41:55 +0200

    start of lexeris

--- src/parsing/utils/lexeris.c
+++ src/parsing/utils/lexeris.c
@@ -4,32 +4,44 @@
 
 #define READ_CHUNK_SIZE KB(1)
 
+typedef enum GTokenType{
+    RULE_NAME,
+    TERMINAL,
+    NON_TERMINAL,
+    PUNCTUATION
+} GTokenType;
+//grammar token
+typedef struct GToken
+{
+    char* value;
+    GTokenType type;
+    
+} GToken;
+
+
 int main()
 {
 
     ERROR_LOG("TEST ERR");
     LSEArena* filearena = arena_create(KB(10));
-    LSEFileHandle file = open_file_handle("E:/Projects/C/LegacySearchEngine/src/parsing/LR1/gramkmar.gmr");
-    // char* readBufferWindow = arena_alloc(&filearena, READ_CHUNK_SIZE);
-    // if(readBufferWindow == NULL){
-    //     printf("Failed to allocate memory");
-    //     return -1;
-    // }
-    // size_t eof = 0;
-    // while((eof = read_bufferchunk(&file, readBufferWindow, READ_CHUNK_SIZE)) != 0){
-    //     printf("Reading chunk: %s", readBufferWindow);
-    // }
-    // double *p;
+    LSEFileHandle file = open_file_handle("E:/Projects/C/LegacySearchEngine/src/parsing/LR1/grammar.gmr");
+    char* readBufferWindow = arena_alloc(filearena, READ_CHUNK_SIZE);
+    if(readBufferWindow == NULL){
+        printf("Failed to allocate memory");
+        return 1;
+    }
+    LSEArena* tokens_mem = arena_create(KB(10));
+    char* tokens = dyna_init(tokens_mem, 200, sizeof(GToken));
 
-
-    // WARNING_LOG("TEST WARN");
-    // INFO_LOG("TEST INFO");
-
-    // int a = 1;
-    // LSE_ASSERT(a == 6, "a should be 6");
-    
-    printf("read done");
-    
-
+    size_t bytesRead = 0;
+    unsigned long buffer_cursor = 0;
+    while((bytesRead = read_bufferchunk(&file, readBufferWindow, READ_CHUNK_SIZE)) != 0){
+        INFO_LOG("Reading chunk: %s", readBufferWindow);
+        for (size_t i = 0; i < bytesRead; i++)
+        {
+            //lexer
+        }
+    }
+   
 
 }
